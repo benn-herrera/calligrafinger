dd44a7f51e70e62a41164df3c65be50c40a5456d
Benn Herrera <benn.herrera@pm.me>
2024-11-17 19:31:00 -0800

    used latest compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.tinybitsinteractive.calligrafinger"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tinybitsinteractive.calligrafinger"
